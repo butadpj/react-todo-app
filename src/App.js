b99945dc5ce57cd1f760e024db424b1907a63a34
butadpj <butadpj@gmail.com>
2021-09-29 12:08:10 +0800

    Create separate components for Header, Form, and TodoList

--- src/App.js
+++ src/App.js
@@ -1,10 +1,57 @@
 import './App.css';
 
+const Header = (props) => <header>{props.children}</header>
+
+const TodoList = (props) => {
+  return (
+    <ul className="todo__list">
+      {props.todos.map((todo) => {
+        return (
+          <li key={todo.id}>
+            {todo.name}
+            <div>
+              <button 
+                className="btn done-btn"
+                onClick={() => props.toggleIsDone(todo)}
+              >
+                Done
+              </button> 
+              <button 
+                className="btn delete-btn"
+                onClick={() => props.deleteTodo(todo.id)}
+              >
+                Delete
+              </button>
+            </div>
+          </li> 
+        )
+      })}
+  </ul>
+  )
+}
+
+const TodoForm = (props) => {
+  return (
+    <form className="todo-form" onSubmit={props.onSubmit}>
+      <input 
+        className="todo__input" 
+        type="text" 
+        value={props.value} 
+        placeholder="Add your new todo" 
+        onChange={props.onChange}
+      />
+      <button type="submit" className="add-btn btn">Add</button>
+    </form>
+  )
+}
+
 const App = () => {
-  
   return (
     <div className="app">
-      <h2>Hello World</h2>
+      <Header>Todo App</Header>
+      <br />
+      <TodoForm />
+      <TodoList />
     </div>
   );
 }
